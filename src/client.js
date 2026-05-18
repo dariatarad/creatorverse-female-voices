a78e3c48fd690f280cd08c6d4039c28b648d8f78
dariatarad <108838898+dariatarad@users.noreply.github.com>
2026-05-17 19:15:04 -0700

    !fix(api): update supabase key

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const URL = 'https://cyiclziaoivdvoanvfdz.supabase.co'
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5aWNsemlhb2l2ZHZvYW52ZmR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MDUyMzksImV4cCI6MjA5NDM4MTIzOX0.pKQe8dkAKsiIoyDlcB3Y7sgG0bkU0LFIyVeZmKnCei4'
+const API_KEY = 'sb_publishable_h6OcsP1sBq8lKDMtW1cxuQ_PF_R4dUJ'
 
 export const supabase = createClient(URL, API_KEY)
